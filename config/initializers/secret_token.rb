f4eb43863b5d797dc75857adff990ca1c8b42844
Julio Daniel Joachin M <kobe8323@MacBook-Pro-de-Porfirio-Marco-Agustin-Chavez.local>
2011-02-24 10:50:48 -0600

    lab5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab5::Application.config.secret_token = '0e98ca5565f0b7a1447219ba6c9ccca864f421cb18c7eaeb6d91b61a3bf9d44bea98f6ed9a4b760397c1746531be2e92ccb00a9a6d052c50e5dfcd173f9917de'
